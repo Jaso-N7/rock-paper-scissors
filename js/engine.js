f25e1db6e9b1d7eb475530d9f39b217b25a403e2
Jason Robinson <jason.robinson>
2024-06-29 21:28:53 -0500

    feat: apply decision logic for computer to play
    The game is against the computer, so this ability ensures that the
    computer randomly "shoots" during gameplay.

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,26 @@
 console.log("Ready Player One")
+
+/*
+   A Choice is one of:
+   - "rock"
+   - "paper"
+   - "scissors"
+   INTERPRETATION: Represents the decision made in the game
+   WHERE: Choice is a String
+ */
+
+// getComputerChoice : Choice
+// Randomly returns "rock", "paper", or "scissors"
+let getComputerChoice = () => {
+    
+    const signal = (Math.random() * 10)
+
+    if (signal >= 0 && signal < 4) {
+	return "rock"
+    } else if (signal >= 4 && signal < 7) {
+	return "paper"
+    } else if (signal >= 7 && signal <= 10) {
+	return "scissors"
+    }
+	
+}
