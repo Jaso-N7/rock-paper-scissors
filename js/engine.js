cefccbd7709687536355dcc56e41f6977f3264fe
Jason Robinson <jason.robinson>
2024-07-11 17:28:23 -0500

    feat!: add logic to play a single round

--- js/engine.js
+++ js/engine.js
@@ -13,7 +13,7 @@ console.log("Ready Player One")
 
 // getComputerChoice : Choice
 // Randomly returns "rock", "paper", or "scissors"
-const getComputerChoice = () => {
+function getComputerChoice() {
     
     const signal = (Math.random() * 10)
 
@@ -29,15 +29,58 @@ const getComputerChoice = () => {
 
 // getHumanChoice : Choice
 // Returns one of the valid choices based on the user input
-const getHumanChoice = () => {
+function getHumanChoice() {
 
-    const handSignal = prompt("Rock, Paper, or Scissors?").toLowerCase()
+    const handSignal = 
+	prompt("Rock, Paper, or Scissors?").toLowerCase()
 
     if (handSignal === "rock" || handSignal === "paper" ||
-	handSignal === "paper") {
+	handSignal === "scissors") {
 	return handSignal
     } else {
 	alert("Bad sign, go again")
 	getHumanChoice()
     }
 }
+
+function convertChoice (c) {
+    return (c == "rock") ? 1 : (c == "paper") ? 2 : 3
+}
+
+// playRound : Choice Choice -> IO
+// Takes the human and computer player choices and determines the
+// winner of the round via a string message.
+function playRound(humanChoice, computerChoice) {
+
+    let hc = 0
+    let cc = 0
+    
+    if (computerChoice === "scissors" &&
+	humanChoice === "rock") {
+	hc = convertChoice(computerChoice)
+	cc = convertChoice(humanChoice)
+    } else if (humanChoice === "scissors" &&
+	       computerChoice === "rock"){
+	cc = convertChoice(humanChoice)
+	hc = convertChoice(computerChoice)
+    } else {
+	hc = convertChoice(humanChoice)
+	cc = convertChoice(computerChoice)
+    }
+
+    if (hc > cc) {
+	humanScore += 1
+	console.log( `You win! ${humanChoice} beats ${computerChoice}` )
+    } else if (cc > hc) {
+	computerScore += 1
+	console.log( `You lose! ${computerChoice} beats ${humanChoice}` )
+    } else {
+	console.log( `Tied! ${humanChoice} and ${computerChoice}` )
+    }
+
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound( humanSelection, computerSelection )
